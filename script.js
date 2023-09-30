fb48c3073fb3754850edf5f4e7df8304e7fa7cde
yasoo-exe <76653084+yasoo-exe@users.noreply.github.com>
2023-09-30 17:54:49 +0500

    styling of grid and buttons
    colors/bg-colors and icons added, flexbox alignment modified

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-let button = document.querySelector(".controls input[type='button']");
+let button = document.querySelector(".controls button[type='button']");
 let container = document.querySelector(".container");
 
 function defaultGrid() {
   for (let i = 0; i < Math.pow(16, 2); i++) {
     container.appendChild(document.createElement("div"));
   }
+  innerDivs(16);
 }
 function makeGrid() {
   container.innerText = "";
@@ -13,10 +14,27 @@ function makeGrid() {
     for (let i = 0; i < Math.pow(gridSize, 2); i++) {
       container.appendChild(document.createElement("div"));
     }
+    innerDivs(gridSize);
   } else {
     alert("Please enter a number between 1-100!");
     defaultGrid();
   }
+}
+
+function innerDivs(arg) {
+  let divs = document.querySelectorAll(".container div");
+  Array.from(divs).map((x) => {
+    x.style.height = `${500 / arg}px`;
+    x.style.width = `${500 / arg}px`;
+    x.addEventListener("mouseover", randomizeColor, { once: true });
+  });
+}
+
+function randomizeColor() {
+  let red = Math.floor(Math.random() * 256);
+  let green = Math.floor(Math.random() * 256);
+  let blue = Math.floor(Math.random() * 256);
+  this.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 }
 
 document.addEventListener("DOMContentLoaded", defaultGrid);
